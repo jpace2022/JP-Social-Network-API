6a54c7db266a39efbc685459f8a3c1060b0ec984
James Pace <pacej2022@gmail.com>
2022-11-25 18:45:41 -0800

    added user control code. Incomplete

---
+++ controllers/users-controller.js
@@ -0,0 +1,26 @@
+const {Users} = require('../models');
+
+const usersController = {
+    createUsers({body}, res) {
+        Users.create(body)
+        .then(dbUsersData => res.json(dbUsersData))
+        .catch(err => res.status(400).json(err));
+    },
+
+    getAllUsers(req, res) {
+        Users.find({})
+        .populate({path: 'thoughts', select: '-__v'})
+        .populate({path: 'friends', select: '-__v'})
+        .select('-__v')
+        .then(dbUsersData => res.json(dbUsersData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+
+    getUsersById({params}, res) {
+        Users.findOne({_id: params.id })
+        
+    }
+}
